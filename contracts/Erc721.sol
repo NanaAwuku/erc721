4aeb1f07e5af3e3ee40dfdddc223672784804113
Nana Awuku <apatrickadjei@gmail.com>
2023-09-06 10:34:22 +0000

    new commit

--- contracts/Erc721.sol
+++ contracts/Erc721.sol
@@ -1,12 +1,21 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-contract Erc721{
+contract MyNFT {
+    uint256 private _totalSupply = 1000;
 
+    // name
+    function name() public pure returns (string memory) {
+        return "My NFT";
+    }
 
+    // symbol
+    function symbol() public pure returns (string memory) {
+        return "MNFT";
+    }
 
-
-
-
+    // total supply
+    function totalSupply() public view returns (uint256) {
+        return _totalSupply;
+    }
 }
-
